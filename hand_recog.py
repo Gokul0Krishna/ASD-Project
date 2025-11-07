00525b47a474cafb2fd78f2d8a106bfc5b06918e
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2025-11-07 12:10:23 +0530

    final code testsite setup

--- hand_recog.py
+++ hand_recog.py
@@ -1,32 +1,16 @@
-from model import Model
-import numpy as np
-
-class Detect():
+import cv2
+class Idk():
     def __init__(self):
-        model = Model()
-        self.hands,self.screen_height,self.screen_width = model.initialize_model(num_of_hands=1,confidence_score=0.7)
-        self.smoothening = 5
-        self.click_threshold = 30 
-        self.click_down = False
+        pass
 
-    def predict(self,rgb,h,w):
-        'predicts if fingers click or not inputs:frame,height and width'
-        results = self.hands.process(rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                lm = hand_landmarks.landmark
-                index_finger = lm[8]
-                thumb = lm[4]
-                x = int(index_finger.x * w)
-                y = int(index_finger.y * h)
-
-                screen_x = np.interp(x, (100, w-100), (0, self.screen_width))
-                screen_y = np.interp(y, (100, h-100), (0, self.screen_height))
-
-                thumb_x, thumb_y = int(thumb.x * w), int(thumb.y * h)
-                distance = np.hypot(x - thumb_x, y - thumb_y)
-
-                if distance < self.click_threshold:
-                    return True,x,y,
-                else:
-                    return False
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1) 
+        h, w, _ = frame.shape
+        cv2.imshow('Virtual Mouse', frame)
+        if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to quit
+                break
+    cap.release()
+    cv2.destroyAllWindows()
